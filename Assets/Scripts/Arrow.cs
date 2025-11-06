acb57a459d25a16b0727c5b21893b27e6f60d98e
hackerman2222 <97752735+hackerman2222@users.noreply.github.com>
2025-11-05 23:43:16 -0500

    Ajustement Gym 4

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public float lifetime = 10f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
