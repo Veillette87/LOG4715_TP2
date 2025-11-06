4c10cb1509abfb0eeb7c368a730dc5f1f5896a8c
hackerman2222 <97752735+hackerman2222@users.noreply.github.com>
2025-11-05 23:23:25 -0500

    Ajout gym dalle piege

---
+++ Assets/Arrow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
