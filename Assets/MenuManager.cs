4a59dfbd725dd43639c79bf1736ce29f9ce6e2f6
hackerman2222 <97752735+hackerman2222@users.noreply.github.com>
2025-12-01 23:39:19 -0500

    debut menu

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -4,9 +4,34 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject startButton;
+    public GameObject ControlsButton;
+    public GameObject quitButton;
+
+    public void Start()
+    {
+        setButtonsActive(true);
+    }
     public void GameStart()
     {
         string sceneName = "Gym_TP3_level";
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowControls()
+    {
+        setButtonsActive(false);
+        
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void setButtonsActive(bool isActive)
+    {
+        startButton.SetActive(isActive);
+        ControlsButton.SetActive(isActive);
+        quitButton.SetActive(isActive);
     }
 }
