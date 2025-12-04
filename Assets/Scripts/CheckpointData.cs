cf5ce69ed075431cdcda5b9a2f75b839d1e05887
Paul Besse <paul.besse17@gmail.com>
2025-12-04 18:52:04 -0500

    fixed fade in and checkpoint

--- Assets/Scripts/CheckpointData.cs
+++ Assets/Scripts/CheckpointData.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public static class CheckpointData
 {
     public static bool hasCheckpoint = false;
     public static Vector3 checkpointPos;
 
+    public static List<string> solvedPuzzles = new List<string>(); // énigmes résolues
+
     public static void ResetData()
     {
         hasCheckpoint = false;
         checkpointPos = Vector3.zero;
+        solvedPuzzles.Clear();
     }
 }
